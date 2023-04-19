a5c425f46e326a8d357b1665709f2fc35d105755
aCt0802 <0802.kaz@gmail.com>
2023-04-20 02:28:06 +0900

    Revert "サインイン処理疎通"
    This reverts commit 0fbb727042ea5be9bb24f3e9b627e74efa9008dd.

--- sql/data/user.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `jstutorial`.`user` (`id`, `display_name`) VALUES 
-('9wq03bd469PSsxarykWXk5buO0F2', 'kaz')
-;
--- src/domain/entity/userCredential.ts
+++
@@ -1,4 +0,0 @@
-export type UserCredential = {
-    password: string
-    email: string
-}
